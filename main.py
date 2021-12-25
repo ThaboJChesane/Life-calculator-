48923b536ee2cf7f7c5d9de68bb42e146c2de149
ThaboJChesane <92906455+ThaboJChesane@users.noreply.github.com>
2021-12-25 17:01:31 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+age = input("What is your current age?")
+
+
+
+yearsleft = 90 - int(age)
+x = yearsleft * 365
+y = yearsleft * 52
+z = yearsleft * 12
+
+print(f"You have {x} days, {y} weeks, and {z} months left")
